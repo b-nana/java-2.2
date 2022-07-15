00e3ee5ee5703930506453a976a812cbe26f49aa
b-nana <babenkoaa97@gmail.com>
2022-07-15 12:35:26 +0300

    Fix

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int account = 100;
-        int refillSum = 203;
+        int refillSum = 1100;
         int bonus;
 
-        bonus = refillSum / 100;
-        account = account + bonus;
+        if (refillSum > 1000)  {
+            bonus = refillSum / 100;
+        } else {
+            bonus = 0;
+        }
+
+        account = account + refillSum + bonus;
 
         System.out.println("Количество бонусов равно " + bonus + " рублей.");
         System.out.println("Итоговый счёт " + account + " рублей.");
